fef4b66ad6586585feebb92acd4857a1b75e395b
Hermann D. Schimpf <hschimpf@hds-solutions.net>
2021-04-12 10:47:39 -0400

    Single definition of model rules

--- app/Models/X_Customer.php
+++ app/Models/X_Customer.php
@@ -21,22 +21,12 @@ abstract class X_Customer extends Base\Model {
         'gender',
     ];
 
-    protected static array $createRules = [
+    protected static array $rules = [
         'firstname'     => [ 'required' ],
         'lastname'      => [ 'sometimes', 'nullable' ],
         'ftid'          => [ 'required' ],
         'business_name' => [ 'sometimes', 'nullable' ],
         'email'         => [ 'sometimes', 'nullable', 'email' ],
-        'phone'         => [ 'sometimes', 'nullable' ],
-        'gender'        => [ 'sometimes', 'nullable', 'in:male,female' ],
-    ];
-
-    protected static array $updateRules = [
-        'firstname'     => [ 'required' ],
-        'lastname'      => [ 'sometimes', 'nullable' ],
-        'ftid'          => [ 'required' ],
-        'business_name' => [ 'sometimes', 'nullable' ],
-        'email'         => [ 'required', 'email' ],
         'phone'         => [ 'sometimes', 'nullable' ],
         'gender'        => [ 'sometimes', 'nullable', 'in:male,female' ],
     ];
