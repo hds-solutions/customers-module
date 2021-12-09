0ba4c05f5938d6a8b19213dd2fbe8ed62c71bfaf
Hermann D. Schimpf <hschimpf@hds-solutions.net>
2021-12-09 15:04:20 -0300

    Add Provider.invoices and payments relations

--- app/Models/Provider.php
+++ app/Models/Provider.php
@@ -2,6 +2,17 @@
 
 namespace HDSSolutions\Laravel\Models;
 
+use HDSSolutions\Laravel\Traits\HasPayments;
+
 class Provider extends X_Provider {
+    use HasPayments;
+
+    public function invoices() {
+        return $this->morphMany(Invoice::class, 'partnerable');
+    }
+
+    public function creditNotes() {
+        return $this->hasManyPayments(CreditNote::class);
+    }
 
 }
